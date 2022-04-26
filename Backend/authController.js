8c3179b027a6033505fa93835f9d6bfd8327e8c8
prachi2399 <prachi9239soni@gmail.com>
2022-04-27 02:01:58 +0530

    Update authController.js

--- Backend/authController.js
+++ Backend/authController.js
@@ -1,8 +1,9 @@
 const express= require('express');
-const userModel = require('./userModel');
+const userModel = require('./model/userModel');
+const otpModel = require('./model/OTPModel');
 const jwt = require('jsonwebtoken');
-// const { sendMail } = require("../utility/nodemailer");
-// const JWT_KEY=require('../../secrets');
+const { sendMail } = require("../Backend/utility/nodemailer");
+const JWT_KEY="6578shjk87690@#$%fhj";
 const { use } = require('./userRouter');
 
 //signup
@@ -47,9 +48,9 @@ module.exports.login = async function login(req, res) {
           //bcrypt -> compare
           if (user.password == data.password) {
             let uid = user["_id"]; //uid
-            // let token = jwt.sign({ payload: uid }, JWT_KEY);
-            // res.cookie("login", token, { httpOnly: true });
-            // res.cookie('isLoggedIn',true);
+            let token = jwt.sign({ payload: uid }, JWT_KEY);
+            res.cookie("login", token, { httpOnly: true });
+            res.cookie('isLoggedIn',true);
             return res.json({
               message: "User has logged in",
               data: user, // userDetails:data,
@@ -74,62 +75,38 @@ module.exports.login = async function login(req, res) {
         message: err.message,
       });
     }
-  };
-
-//forgetPassword
-module.exports.forgetpassword = async function forgetpassword(req,res){
-    try{
-        let {email} = req.email;
-        let user = await userModel.findOne({email:email});
-        if(user){
-            //createResetToken is used to create a new token
-            const resetToken= user.createResetToken();
-            //https://abc/resetpassword/resettoken
-            let resetPasswordLink = `${req.protocol}://
-            ${req.get('host')}/resetpassword/${resetToken}`
-
-            //send email to the user
-            //nodemailer
-            let obj={
-              resetPasswordLink:resetPasswordLink,
-              email:email
-            }
-            sendMail("resetPassword",obj);
-            return res.json({
-              message: "reset password link sent",
-              data:resetPasswordLink
-            })
-        }else{
-            res.json({
-                message:"Register First"
-            })
-        }
-    }
-    catch(err){
-        res.status(500).json({
-            message:err.message
-        })
-    }
-}
+  }
 
 //resetpassword
 module.exports.resetpassword = async function resetpassword(req,res){
     try{
-        const token = req.params.token;
-        let {password,confirmPassword}=req.body;
-
-        let user = await userModel.findOne({resetToken:token});
+        let mail = req.body.email;
+        let otp = req.body.code;
+        let user = await otpModel.findOne({email:mail,code:otp});
         if(user){
-            //resetPasswordHandler will update user's password in db
-            user.resetPasswordHandler(password, confirmPassword);
-
-            await user.save();
-            res.json({
-                message: "password changed successfully, please login again"
+            let currentTime = new Date().getTime();
+            let diff = user.expireIn-currentTime;
+            if(diff<0){
+              return res.json({
+                message: "OTP Expired",
             });
+            }else{
+              let user=userModel.findOne({email:mail});
+              if(user){
+                let newPassword = req.body.password;
+                await user.findOneAndUpdate({mail,password:newPassword});
+                return res.json({
+                  message: "Password Changed SuccessFully"
+                });
+              }else{
+                return res.json({
+                  message: "Email doesn't Exist"
+                });
+              }
+            }
         }else{
             res.json({
-                message: "user not found"
+                message: "Invalid OTP"
             });
         }
     }catch(err){
@@ -137,5 +114,43 @@ module.exports.resetpassword = async function resetpassword(req,res){
             message:err.message
         })
     }
+}
+
+module.exports.forgetpassword = async function forgetpassword(req,res){
+  try{
+    let email = req.body.email;
+    let user = await userModel.findOne({email});
+    if(user){
+        let otp = Math.floor((Math.random()*10000)+1);
+        console.log(otp);
+        let userExist = otpModel.findOne({email});
+        if(userExist){
+          let del = await otpModel.findOneAndDelete({email});
+          console.log("user exists, deleting it first")
+        }
+        let otpData = new otpModel({
+          email:req.body.email,
+          code:otp,
+          expireIn:new Date().getTime()+300*1000
+        })
+        let otpResp = await otpData.save();
+        let obj={
+          otp:otp,
+          email:email
+        }
+        sendMail("resetOTP",obj);
+        res.json({
+          message: "OTP Send On Email"
+        });
+    }else{
+        res.json({
+            message: "Email doesn't Exist"
+        });
+    }
+}catch(err){
+    res.json({
+        message:err.message
+    })
+}
 }
 

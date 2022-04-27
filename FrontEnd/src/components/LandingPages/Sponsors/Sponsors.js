eb03621318b2e30c025fc59b04b015154ca362bb
prachi2399 <prachi9239soni@gmail.com>
2022-04-27 13:09:43 +0530

    Create Sponsors.js

---
+++ FrontEnd/src/components/LandingPages/Sponsors/Sponsors.js
@@ -0,0 +1,199 @@
+import React from 'react';
+import './Sponsors.css';
+
+const Sponsors = () => {
+  return (
+    <>
+      <section id="sponsors" className="ftco-section_sponsors">
+        <div className="container_sponsors box-container_sponsors">
+          <div className="row_sponsors justify-content-center pt-5">
+            <div className="col-md-7 heading-section_sponsors text-center ftco-animate_sponsors">
+              <h3 className="subheading" style={{ color: '#6db549' }}>
+                The People Who Have Our Back
+              </h3>
+              <h2 className="mb-1">Our Sponsors</h2>
+            </div>
+          </div>
+          {/* <hr style="margin-left: 30px; margin-right: 30px;"/> */}
+          <div className="row_sponsors align-items-baseline justify-content-center pb-5 div-sponsor">
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://www.jkcement.com/">
+                <img
+                  src="/images/sponsors/2018/jkcement.png"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://paytm.com/">
+                <img
+                  src="/images/sponsors/others/paytm.png"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://www.pepsico.com/">
+                <img
+                  src="/images/sponsors/others/pepsi.png"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://in.puma.com/">
+                <img
+                  src="/images/sponsors/others/puma.png"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://www.redbull.com/">
+                <img
+                  src="/images/sponsors/others/redbull.png"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://www.reebok.com/us">
+                <img
+                  src="/images/sponsors/others/reebok.png"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://www.mahindra.com/">
+                <img
+                  src="/images/sponsors/others/mahindra.png"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://www.heromotocorp.com/en-in/">
+                <img
+                  src="/images/sponsors/others/hero.png"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://www.fastrack.in/">
+                <img
+                  src="/images/sponsors/2018/fastrack.jpg"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://www.ntpc.co.in/">
+                <img
+                  src="/images/sponsors/2018/ntpc.png"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://www.sail.co.in/">
+                <img
+                  src="/images/sponsors/2018/sail.png"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://www.iocl.com/">
+                <img
+                  src="/images/sponsors/2018/iocl.png"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://www.hindustanpetroleum.com/">
+                <img
+                  src="/images/sponsors/2018/hpcl.png"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://www.obcindia.co.in/">
+                <img
+                  src="/images/sponsors/2018/orientalbank.jpg"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://www.adityabirla.com/">
+                <img
+                  src="/images/sponsors/others/adityabirla.png"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://www.swiggy.com/">
+                <img
+                  src="/images/sponsors/2018/swiggy.jpg"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://amul.com/">
+                <img
+                  src="/images/sponsors/others/amul.png"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://www.bankofindia.co.in/">
+                <img
+                  src="/images/sponsors/2018/boi.png"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            <div className="col-lg-3 col-md-3 col-4 pt-5">
+              <a href="https://www.ucobank.com/">
+                <img
+                  src="/images/sponsors/2018/ucobank.png"
+                  className="img-fluid_sponsors img-sponsor"
+                  alt="image_alt"
+                />
+              </a>
+            </div>
+            
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Sponsors;

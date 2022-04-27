4d632e38caf16431f21a1c2756e046f6c3cf0d0c
prachi2399 <prachi9239soni@gmail.com>
2022-04-27 18:43:15 +0530

    Update Events_Components.js

--- FrontEnd/src/components/LandingPages/Events/Events_Components.js
+++ FrontEnd/src/components/LandingPages/Events/Events_Components.js
@@ -4,8 +4,13 @@ import rules from './rules.jpg';
 import trophy from './trophy.jpg';
 import contacts from './contacts.jpg';
 import { FaTimes } from 'react-icons/fa';
-
-
+import BasketballPDF from './pdf/Basketballpayment.pdf'
+import FutsallPDF from './pdf/futsalpayment.pdf'
+import chessPdf from './pdf/chess payment.pdf'
+import CricketPDF from './pdf/cricket payment.pdf'
+import VolleyballPDF from './pdf/volleyball payment.pdf';
+import TTPDF from './pdf/TTpayment.pdf'
+import khokhoPDF from './pdf/khokhopayment.pdf'
 
 const Basketball = (props) => {
   return (
@@ -36,35 +41,17 @@ const Basketball = (props) => {
                   />
                 </div>
                 <div className={`${styles.info}`}>
-                  <h4>Contacts</h4>
-                  <p
-                    className={`${styles.paragraph}`}
-                    style={{ paddingBottom: '20px' }}
-                  >
-                    <b>CAPTAIN</b>
-                    <br />
-                    Aryan Shukla <br />
-                    8171354203
-                    <br />
-                    aryanshukla.mat18@itbhu.ac.in
-                    <br />
-                    <br />
-                    Arya Kumari <b>(W)</b>
-                    <br />
-                    8409372829
-                    <br />
-                    aryakumari.mst18@itbhu.ac.in
-                    <br />
-                    <br />
-                    <b>VICE-CAPTAIN</b>
-                    <br />
-                    Kinshuk Dwivedi
-                    <br />
-                    9425183369
-                    <br />
-                    kinshuk.dwivedi.phy19@itbhu.ac.in
-                    <br />
-                  </p>
+                <div className={`${styles.info} ${styles.rule}`}>
+                    <h4> Rules </h4>
+                    <a
+                      href={BasketballPDF}
+                      target="_blank"
+                      rel="noreferrer"
+                      className={`${styles.rulebook}`}
+                    >
+                      CLICK HERE
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
@@ -105,35 +92,17 @@ const Futsal = (props) => {
                   />
                 </div>
                 <div className={`${styles.info}`}>
-                  <h4>Contacts</h4>
-                  <p
-                    className={`${styles.paragraph}`}
-                    style={{ paddingBottom: '20px' }}
-                  >
-                    <b>CAPTAIN</b>
-                    <br />
-                    Aryan Shukla <br />
-                    8171354203
-                    <br />
-                    aryanshukla.mat18@itbhu.ac.in
-                    <br />
-                    <br />
-                    Arya Kumari <b>(W)</b>
-                    <br />
-                    8409372829
-                    <br />
-                    aryakumari.mst18@itbhu.ac.in
-                    <br />
-                    <br />
-                    <b>VICE-CAPTAIN</b>
-                    <br />
-                    Kinshuk Dwivedi
-                    <br />
-                    9425183369
-                    <br />
-                    kinshuk.dwivedi.phy19@itbhu.ac.in
-                    <br />
-                  </p>
+                <div className={`${styles.info} ${styles.rule}`}>
+                    <h4> Payment & Contact Detail </h4>
+                    <a
+                      href={FutsallPDF}
+                      target="_blank"
+                      rel="noreferrer"
+                      className={`${styles.rulebook}`}
+                    >
+                      CLICK HERE
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
@@ -176,30 +145,17 @@ const Chess = (props) => {
                   />
                 </div>
                 <div className={`${styles.info}`}>
-                  <h4>Contacts</h4>
-                  <p
-                    className={`${styles.paragraph}`}
-                    style={{ paddingBottom: '12px' }}
-                  >
-                    <b>CAPTAIN</b>
-                    <br />
-                    Mack Mohan Verma <br />
-                    9548338599
-                    <br />
-                    mack.mohanverma.mec19@itbhu.ac.in
-                    <br />
-                    <br />
-                    <b>VICE-CAPTAIN</b>
-                    <br />
-                    Shaurya Panpalia <br />
-                    7877860896
-                    <br />
-                    shaurya.panpalia.mst19@itbhu.ac.in
-                    <br />
-                    <br />
-                    <br />
-                    {/* <br /> */}
-                  </p>
+                <div className={`${styles.info} ${styles.rule}`}>
+                    <h4> Payment & Contact Detail </h4>
+                    <a
+                      href={chessPdf}
+                      target="_blank"
+                      rel="noreferrer"
+                      className={`${styles.rulebook}`}
+                    >
+                      CLICK HERE 
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
@@ -239,30 +195,17 @@ const Cricket = (props) => {
                   />
                 </div>
                 <div className={`${styles.info}`}>
-                  <h4>Contacts</h4>
-                  <p
-                    className={`${styles.paragraph}`}
-                    style={{ paddingBottom: '15px' }}
-                  >
-                    <b>CAPTAIN</b>
-                    <br />
-                    Shashank S Prakash <br />
-                    8368920987
-                    <br />
-                    shashanksp.met18@iitbhu.ac.in
-                    <br />
-                    <br />
-                    <b>VICE-CAPTAIN</b>
-                    <br />
-                    Satyendra Kumar Satyarthi <br />
-                    9877437762
-                    <br />
-                    satyendraks.rs.cer17@itbhu.ac.in
-                    <br />
-                    <br />
-                    <br />
-                    {/* <br /> */}
-                  </p>
+                <div className={`${styles.info} ${styles.rule}`}>
+                    <h4> Payment & Contact Detail </h4>
+                    <a
+                      href={CricketPDF}
+                      target="_blank"
+                      rel="noreferrer"
+                      className={`${styles.rulebook}`}
+                    >
+                      CLICK HERE
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
@@ -304,43 +247,17 @@ const Khokho = (props) => {
                   />
                 </div>
                 <div className={`${styles.info}`}>
-                  <h4>Contacts</h4>
-                  <p
-                    className={`${styles.paragraph}`}
-                    style={{ paddingBottom: '10px' }}
-                  >
-                    <b>CAPTAIN</b>
-                    <br />
-                    Rahul Lakra
-                    <br />
-                    8877441658
-                    <br />
-                    rahul.lakra.min19@itbhu.ac.in
-                    <br />
-                    <br />
-                    Tejswinee Karwar <b>(W)</b>
-                    <br />
-                    7770880674
-                    <br />
-                    tejswinee.karwar.met19@itbhu.ac.in
-                    <br />
-                    <br />
-                    <b>VICE-CAPTAIN</b>
-                    <br />
-                    Munavath Prashanth
-                    <br />
-                    6301984558
-                    <br />
-                    munavath.prashanth.min19@itbhu.ac.in
-                    <br />
-                    <br />
-                    Jayshree Das <b>(W)</b>
-                    <br />
-                    6000805732
-                    <br />
-                    jayshree.das.min19@itbhu.ac.in
-                    <br />
-                  </p>
+                <div className={`${styles.info} ${styles.rule}`}>
+                    <h4> Payment & Contact Detail </h4>
+                    <a
+                      href={khokhoPDF}
+                      target="_blank"
+                      rel="noreferrer"
+                      className={`${styles.rulebook}`}
+                    >
+                      CLICK HERE
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
@@ -381,29 +298,17 @@ const TableTennis = (props) => {
                   />
                 </div>
                 <div className={`${styles.info}`}>
-                  <h4>Contacts</h4>
-                  <p
-                    className={`${styles.paragraph}`}
-                    style={{ paddingBottom: '12px' }}
-                  >
-                    <b>CAPTAIN</b>
-                    <br />
-                    Sachin Arya <br />
-                    7073524926
-                    <br />
-                    sachinarya.mec18@iitbhu.ac.in
-                    <br />
-                    <br />
-                    Kajal Pathriyal <b>(W)</b>
-                    <br />
-                    8127141299
-                    <br />
-                    kajal.pathriyal.eee19@itbhu.ac.in
-                    <br />
-                    <br />
-                    <br />
-                    {/* <br /> */}
-                  </p>
+                <div className={`${styles.info} ${styles.rule}`}>
+                    <h4> Payment & Contact Detail </h4>
+                    <a
+                      href={TableTennis}
+                      target="_blank"
+                      rel="noreferrer"
+                      className={`${styles.rulebook}`}
+                    >
+                      CLICK HERE
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
@@ -444,40 +349,17 @@ const Volleyball = (props) => {
                   />
                 </div>
                 <div className={`${styles.info}`}>
-                  <h4>Contacts</h4>
-                  <p
-                    className={`${styles.paragraph}`}
-                    style={{ paddingBottom: '10px' }}
-                  >
-                    <b>CAPTAIN</b>
-                    <br />
-                    Shivkaran Singh Ranawat <br />
-                    8824018065
-                    <br />
-                    shivkarans.ranawat.phe19@itbhu.ac.in <br />
-                    <br />
-                    Yashika lamba <b>(W)</b>
-                    <br />
-                    8076365550
-                    <br />
-                    yashika.lamba.phe19@itbhu.ac.in
-                    <br />
-                    <br />
-                    <b>VICE-CAPTAIN</b>
-                    <br />
-                    Mukul jaiman <br />
-                    8302122511
-                    <br />
-                    mukul.jaiman.civ19@itbhu.ac.in
-                    <br />
-                    <br />
-                    Priya Rathore <b>(W)</b>
-                    <br />
-                    8225934951
-                    <br />
-                    priya.rathore.cer19@itbhu.ac.in
-                    <br />
-                  </p>
+                <div className={`${styles.info} ${styles.rule}`}>
+                    <h4> Payment & Contact Detail </h4>
+                    <a
+                      href={VolleyballPDF}
+                      target="_blank"
+                      rel="noreferrer"
+                      className={`${styles.rulebook}`}
+                    >
+                      CLICK HERE 
+                    </a>
+                  </div>
                 </div>
               </div>
             </div>
